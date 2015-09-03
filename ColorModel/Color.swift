2d56087038666c907f25d8e890061f390ae55749
Nil Point <nilpoint@hotmail.com>
2015-09-03 17:09:31 +0800

    Add the Color.swift for the model.

---
+++ ColorModel/Color.swift
@@ -0,0 +1,19 @@
+//
+//  Color.swift
+//  ColorModel
+//
+//  Created by John Alstru on 9/3/15.
+//  Copyright (c) 2015 nilpoint.sample. All rights reserved.
+//
+
+import UIKit
+
+class Color {
+  var hue: Float = 0.0
+  var saturation: Float = 0.0
+  var brightness: Float = 0.0
+  
+  var color: UIColor {
+    return UIColor(hue: CGFloat(hue/360), saturation: CGFloat(saturation/100), brightness: CGFloat(brightness/100), alpha: 1.0)
+  }
+}
