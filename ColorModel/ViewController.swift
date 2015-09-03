2f0c3f2fc055e684a645f0506f2aa1d8fca26eef
Nil Point <nilpoint@hotmail.com>
2015-09-03 18:35:23 +0800

    Fill out the controller.

--- ColorModel/ViewController.swift
+++ ColorModel/ViewController.swift
@@ -9,6 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var colorModel = Color()
+  @IBOutlet var colorView: UIView!
+  
+  @IBAction func changeHue(sender: AnyObject) {
+    if let slider = sender as? UISlider {
+      colorModel.hue = slider.value
+      colorView.backgroundColor = colorModel.color
+    }
+  }
+  
+  @IBAction func changeSaturation(sender: AnyObject) {
+    if let slider = sender as? UISlider {
+      colorModel.saturation = slider.value
+      colorView.backgroundColor = colorModel.color
+    }
+  }
+  
+  @IBAction func changeBrightness(sender: AnyObject) {
+    if let slider = sender as? UISlider {
+      colorModel.brightness = slider.value
+      colorView.backgroundColor = colorModel.color
+    }
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
