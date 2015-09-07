8599c4a507a139de2e387f16d328459f7aa9bb7f
Nil Point <nilpoint@hotmail.com>
2015-09-07 15:16:40 +0800

    Make the Color class eligible for KVO.

--- ColorModel/Color.swift
+++ ColorModel/Color.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class Color {
-  var hue: Float = 0.0
-  var saturation: Float = 0.0
-  var brightness: Float = 0.0
+class Color: NSObject {
+  dynamic var hue: Float = 0.0
+  dynamic var saturation: Float = 0.0
+  dynamic var brightness: Float = 0.0
   
   var color: UIColor {
     return UIColor(hue: CGFloat(hue/360), saturation: CGFloat(saturation/100), brightness: CGFloat(brightness/100), alpha: 1.0)
